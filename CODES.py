b9d80942d506284473b0c9bf8fac1ec86d1f1a0b
= <benleong0@gmail.com>
2021-05-25 22:14:51 +0100

    arrays

---
+++ CODES.py
@@ -0,0 +1,5 @@
+white_pieces = ["P", "R", "N", "B", "K", "Q"]
+black_pieces = ["p", "r", "n", "b", "k", "q"]
+
+ranks = ["a", "b", "c", "d", "e", "f", "g", "h"]
+files = ["8", "7", "6", "5", "4", "3", "2", "1"]
